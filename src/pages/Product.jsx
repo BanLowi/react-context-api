c09331be7d366b4d17cd92c9dc0ddcc2c12f2809
BanLowi <lowi40k@gmail.com>
2025-11-18 15:26:19 +0100

    added context global states and static array for products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 import { useParams, Link } from "react-router-dom"
 import axios from "axios"
 
-import Card from "../components/Card"
 
 export default function Product() {
 
